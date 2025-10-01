495fd8042a9b7a977e50a96ed3456ae13fe54859
Natitorresg <nltorresg@uc.cl>
2025-10-01 00:45:16 -0300

    E1: RF04 - casi listo

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 const app = require("./app");
 const db = require("../models");
-const dotenv = require("dotenv");
-
-dotenv.config();
+//const dotenv = require("dotenv");
+//dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
--- src/api/routes.js
+++ src/api/routes.js
@@ -1,8 +1,14 @@
 const Router = require("@koa/router");
 const properties = require("./routes/properties.js");
+const wallet = require("./routes/wallet.js");
+const purchases = require("./routes/purchases.js");
 
 const router = new Router();
+router.get('/', (ctx) => { ctx.body = { ok: true }; });
+
 
 router.use('/properties', properties.routes());
+router.use('/wallet', wallet.routes());
+router.use('/purchases', purchases.routes());   
 
 module.exports = router;
